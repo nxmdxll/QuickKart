83d2503078d60b8ec16d8821a4ae9cd80a68a515
Nxmdxll <tenzinn050@gmail.com>
2025-06-19 17:52:03 +0530

    ✨ Initial commit — Login by Tenzin

---
+++ src/main/java/com/mycompany/loginapp/LoginApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginapp;
+
+/**
+ *
+ * @author tenzi
+ */
+public class LoginApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
